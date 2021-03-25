c0517d276890ff0652019288c8ad824bd4549920
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-03-25 23:40:40 +0200

    add_goods_from_db

--- js/main.js
+++ js/main.js
@@ -43,6 +43,19 @@ document.addEventListener('keydown', (e) => {
 
 // scroll smooth
 
+const scroll = (elem) => {
+	let id = elem.getAttribute('href');
+
+	if (id === null || id === '#') {
+		id = '#body'
+	}
+
+	document.querySelector(id).scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	})
+}
+
 {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
@@ -60,12 +73,121 @@ document.addEventListener('keydown', (e) => {
 	scrollLinks.forEach(item => {
 		item.addEventListener('click', (event) => {
 			event.preventDefault();
-			const id = item.getAttribute('href');
-			document.querySelector(id).scrollIntoView({
-				behavior: 'smooth',
-				block: 'start'
-			})
+			scroll(item);
 		})
 	});
 
 }
+
+
+// goods
+
+const more = document.querySelector('.more'),
+	navigationLink = document.querySelectorAll('.navigation-link'),
+	longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async () => {
+	const result = await fetch('db/db.json');
+
+	if (!result.ok) {
+		throw 'Error :(' + result.status
+	}
+
+	return await result.json();
+}
+
+// fetch('db/db.json')
+// 	.then(response => {
+// 		return response.json()
+// 	})
+// 	.then(data => {
+// 		console.log(data)
+// 	})
+
+const createCard = ({ description, label, img, name, id, price }) => {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
+
+	card.innerHTML = `
+	                <div class="goods-card">
+					    ${label ? `<span class="label">${label}</span>` : ''}
+						<img src="db/${img}" alt="${name}" class="goods-image">
+						<h3 class="goods-title">${name}</h3>
+						<p class="goods-description">${description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="${id}">
+							<span class="button-price">${price}</span>
+						</button>
+					</div>
+	`;
+
+	return card;
+};
+
+const renderCards = data => {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard);
+
+	// cards.forEach(item => {
+	// 	longGoodsList.append(item);
+	// }
+	longGoodsList.append(...cards);
+
+	document.body.classList.add('show-goods')
+};
+
+
+more.addEventListener('click', (event) => {
+	event.preventDefault();
+
+	getGoods().
+		then(renderCards);
+
+	scroll(event.target);
+});
+
+const filterCard = (field, value) => {
+	getGoods()
+		.then(data => {
+			const filteredGoods = data.filter(good => {
+				return good[field] === value
+			});
+			if (filteredGoods.length == 0) {
+				return data
+			}
+			return filteredGoods;
+		})
+		.then(renderCards);
+};
+
+navigationLink.forEach(item => {
+	item.addEventListener('click', event => {
+		event.preventDefault();
+
+		const field = item.dataset.field;
+		const value = item.textContent;
+		console.log(field);
+		console.log(value);
+
+		filterCard(field, value);
+	})
+})
+
+const btnAccessor = document.querySelector('.card-1  .button'),
+	btnClothing = document.querySelector('.card-2  .button');
+
+
+const btnClick = (btn, field, value) => {
+	filterCard(field, value);
+	scroll(btn);
+}
+
+btnAccessor.addEventListener('click', () => {
+	btnClick(btnAccessor, 'category', 'Accessories');
+});
+
+btnClothing.addEventListener('click', () => {
+	btnClick(btnClothing, 'category', 'Clothing');
+});
+
+
+
